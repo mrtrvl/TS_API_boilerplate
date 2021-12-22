3e665168ffbf5938e94d94fafd2984a9302d20e5
mrtrvl <mrt@mrt.ee>
2021-12-22 11:17:14 +0200

    Update openapi documentation, add swagger-ui

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # TS_API_boilerplate
 TS Node Express API boilerplate with users
+
+# Installation
+git pull https://github.com/mrtrvl/TS_API_boilerplate.git
+
+cd TS_API_boilerplate
+
+npm install
+
+npm start
+
+# API documentation
+http://localhost:4000/api-docs
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,16 @@
 import express, { Application } from 'express';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
 import { pingRouter } from './components/ping';
 import { usersRouter } from './components/users';
 import { notFoundRouter } from './components/notFound';
+import swaggerDocument from '../openapi.json';
 
 const app: Application = express();
 
 // CORS middleware
 app.use(cors());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Json middleware
 app.use(express.json());
